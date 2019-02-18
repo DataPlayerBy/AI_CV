7a361ff9eb707caa2d25af0ba44a317f8fbbb780
gaojingbin <gaojingbin@weiboyi.com>
2019-02-18 17:28:59 +0800

    Add fashion mnist identifier
    增加10种服装图片识别的代码

--- MNIST/__init__.py
+++ MNIST/__init__.py
@@ -1,2 +1,3 @@
 # 手写数字识别
 # 数据集：每张图片为28x28像素的灰度图片的尺寸为784，训练集大小55000，验证集大小5000，测试集大小10000
+# 参考http://www.tensorfly.cn/tfdoc/tutorials/mnist_beginners.html，http://www.tensorfly.cn/tfdoc/tutorials/mnist_pros.html
